07640caa9a537da87661e26c64197870dd5fcca0
Romain Grimau <romain.grimau@hotmail.be>
2025-01-31 13:57:16 +0100

    all lines move independently at a random speed, refactor code

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,7 @@ use std::{thread, time};
 
 const WEIGHT: f32 = 4.0;
 const NBR_OBJECTS: usize = 100;
-const COUNTER_TRESHOLD: u8 = 50;
-
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
+//const COUNTER_TRESHOLD: u8 = 50;
 
 fn main() {
     nannou::app(model).update(update).simple_window(view).run();
@@ -20,10 +13,7 @@ fn main() {
 struct Model {
     // list containing at n a starting point and at n+1 an ending point
     // to draw a line
-    pub lines: [(Point2, Point2); NBR_OBJECTS],
-    pub direction: Direction,
-    pub counter: u8,
-    pub step: f32,
+    pub lines: [(Point2, Point2, f32); NBR_OBJECTS],
 }
 
 fn model(_app: &App) -> Model {
@@ -31,30 +21,22 @@ fn model(_app: &App) -> Model {
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    if model.counter < COUNTER_TRESHOLD {
-        model.move_lines();
-        model.counter += 1;
-    } else {
-        match model.direction {
-            Direction::Up => {
-                model.direction = Direction::Down;
-                model.step = -2.0
-            }
-            Direction::Down => {
-                model.direction = Direction::Left;
-                model.step = 2.0
-            }
-            Direction::Left => {
-                model.direction = Direction::Right;
-                model.step = -2.0
-            }
-            Direction::Right => {
-                model.direction = Direction::Up;
-                model.step = 2.0
-            }
+    for i in 0..NBR_OBJECTS / 2 {
+        if f32::max(model.lines[i].0.y, model.lines[i].1.y) >= 500.0
+            || f32::min(model.lines[i].0.y, model.lines[i].1.y) <= -500.0
+        {
+            model.lines[i].2 = -model.lines[i].2;
         }
-        model.counter = 0;
     }
+
+    for i in NBR_OBJECTS / 2..NBR_OBJECTS {
+        if f32::max(model.lines[i].0.x, model.lines[i].1.x) >= 500.0
+            || f32::min(model.lines[i].1.x, model.lines[i].0.x) <= -500.0
+        {
+            model.lines[i].2 = -model.lines[i].2;
+        }
+    }
+    model.move_lines();
     thread::sleep(time::Duration::from_millis(50));
 }
 
@@ -81,22 +63,27 @@ fn view(app: &App, model: &Model, frame: Frame) {
  * direction: enum representing either horizontal or
  * vertical direction.
  */
-fn generate_lines() -> [(Point2, Point2); NBR_OBJECTS] {
-    let mut lines: [(Vec2, Vec2); NBR_OBJECTS] = [(pt2(0.0, 0.0), pt2(0.0, 0.0)); NBR_OBJECTS];
+fn generate_lines() -> [(Point2, Point2, f32); NBR_OBJECTS] {
+    let mut lines: [(Vec2, Vec2, f32); NBR_OBJECTS] =
+        [(pt2(0.0, 0.0), pt2(0.0, 0.0), 0.0); NBR_OBJECTS];
 
     for (i, line) in lines.iter_mut().enumerate() {
-        let start_x_coord = random_range(-300.0, 300.0);
-        let start_y_coord = random_range(-300.0, 300.0);
-        let line_length = random_range(-300.0, 300.0);
+        let start_x_coord = random_range(-299.0, 299.0);
+        let start_y_coord = random_range(-299.0, 299.0);
+        let line_length = match random::<u8>() % 2 {
+            0 => random_range(30.0, 250.0),
+            1 => random_range(-250.0, -30.0),
+            _ => 50.0,
+        };
 
         let start_point = pt2(start_x_coord, start_y_coord);
         let end_point: Vec2 = if i < NBR_OBJECTS / 2 {
-            pt2(start_x_coord, start_y_coord + line_length) // create vertical line
+            pt2(start_x_coord, start_y_coord + line_length % 300.0) // create vertical line
         } else {
-            pt2(start_x_coord + line_length, start_y_coord) // create horizontal line
+            pt2(start_x_coord + line_length % 300.0, start_y_coord) // create horizontal line
         };
         // TODO:check length of the line and put a low threshold
-        *line = (start_point, end_point);
+        *line = (start_point, end_point, random_range(-4.0, 4.0));
     }
 
     lines
@@ -106,23 +93,20 @@ impl Model {
     fn new() -> Model {
         Model {
             lines: generate_lines(),
-            direction: Direction::Up,
-            counter: 0,
-            step: 2.0,
         }
     }
 
     fn move_lines_vertically(&mut self) {
         for i in 0..NBR_OBJECTS / 2 {
-            self.lines[i].0.y += self.step;
-            self.lines[i].1.y += self.step;
+            self.lines[i].0.y += self.lines[i].2;
+            self.lines[i].1.y += self.lines[i].2;
         }
     }
 
     fn move_lines_horizontally(&mut self) {
         for i in NBR_OBJECTS / 2..NBR_OBJECTS {
-            self.lines[i].0.x += self.step;
-            self.lines[i].1.x += self.step;
+            self.lines[i].0.x += self.lines[i].2;
+            self.lines[i].1.x += self.lines[i].2;
         }
     }
 
